5746c3dba63e645d4c3dd935b934078669c1b8ed
israeldewcom <haddockthor@gmail.com>
2026-09-07 12:38:13 +0100

    Update metricsEndpoint to ignore request parameter

--- src/utils/metrics.ts
+++ src/utils/metrics.ts
@@ -37,7 +37,7 @@ export const metricsMiddleware = (req: Request, res: Response, next: NextFunctio
   next();
 };
 
-export const metricsEndpoint = async (req: Request, res: Response) => {
+export const metricsEndpoint = async (_req: Request, res: Response) => {
   res.set('Content-Type', register.contentType);
   res.end(await register.metrics());
 };
