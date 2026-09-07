9a32129607a4ae76097f798b1c2295ed226a6cd1
israeldewcom <haddockthor@gmail.com>
2026-09-07 12:09:26 +0100

    Update reconciliation.job.ts

--- src/jobs/reconciliation.job.ts
+++ src/jobs/reconciliation.job.ts
@@ -1,6 +1,4 @@
 import { reconciliationQueue } from './queues';
-import { PaymentService } from '../core/payments/payment.service';
-import logger from '../config/logger';
 
 export const startReconciliationJob = () => {
   // Run every 10 minutes
