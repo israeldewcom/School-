eb02e6be196d132464e594084890b56ab3ceda4f
israeldewcom <haddockthor@gmail.com>
2026-09-07 06:35:44 +0100

    Add metrics middleware and endpoint for HTTP requests

---
+++ src/utils/metrics.ts
@@ -0,0 +1,43 @@
+import { Request, Response, NextFunction } from 'express';
+import client from 'prom-client';
+
+// Create a Registry
+const register = new client.Registry();
+
+// Add default metrics
+client.collectDefaultMetrics({ register });
+
+// Custom metrics
+const httpRequestDurationMicroseconds = new client.Histogram({
+  name: 'http_request_duration_ms',
+  help: 'Duration of HTTP requests in ms',
+  labelNames: ['method', 'route', 'status_code'],
+  buckets: [10, 50, 100, 200, 500, 1000, 2000],
+  registers: [register],
+});
+
+const httpRequestTotal = new client.Counter({
+  name: 'http_requests_total',
+  help: 'Total number of HTTP requests',
+  labelNames: ['method', 'route', 'status_code'],
+  registers: [register],
+});
+
+export const metricsMiddleware = (req: Request, res: Response, next: NextFunction) => {
+  const start = Date.now();
+  res.on('finish', () => {
+    const duration = Date.now() - start;
+    const route = req.route ? req.route.path : req.path;
+    httpRequestDurationMicroseconds.observe(
+      { method: req.method, route, status_code: res.statusCode },
+      duration
+    );
+    httpRequestTotal.inc({ method: req.method, route, status_code: res.statusCode });
+  });
+  next();
+};
+
+export const metricsEndpoint = async (req: Request, res: Response) => {
+  res.set('Content-Type', register.contentType);
+  res.end(await register.metrics());
+};
