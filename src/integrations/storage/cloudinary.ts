eb937dafab1a3c01d0a3c6505fae763662c80fa6
israeldewcom <haddockthor@gmail.com>
2026-09-07 13:09:52 +0100

    Update cloudinary.ts

--- src/integrations/storage/cloudinary.ts
+++ src/integrations/storage/cloudinary.ts
@@ -1,7 +1,7 @@
-import cloudinary from 'cloudinary';
+import { v2 as cloudinary } from 'cloudinary';
 import { env } from '../../config/env';
 
-cloudinary.v2.config({
+cloudinary.config({
   cloud_name: env.CLOUDINARY_CLOUD_NAME,
   api_key: env.CLOUDINARY_API_KEY,
   api_secret: env.CLOUDINARY_API_SECRET,
