315f997b387dea8f5286a8c7b644a132caa02287
israeldewcom <haddockthor@gmail.com>
2026-09-07 06:31:20 +0100

    Add custom error classes for application errors

---
+++ src/utils/errors.ts
@@ -0,0 +1,47 @@
+export class AppError extends Error {
+  public statusCode: number;
+  public details?: any;
+
+  constructor(message: string, statusCode: number = 500, details?: any) {
+    super(message);
+    this.statusCode = statusCode;
+    this.details = details;
+    Object.setPrototypeOf(this, AppError.prototype);
+  }
+}
+
+export class ValidationError extends AppError {
+  constructor(message: string, details?: any) {
+    super(message, 400, details);
+  }
+}
+
+export class BadRequestError extends AppError {
+  constructor(message: string) {
+    super(message, 400);
+  }
+}
+
+export class UnauthorizedError extends AppError {
+  constructor(message: string = 'Unauthorized') {
+    super(message, 401);
+  }
+}
+
+export class ForbiddenError extends AppError {
+  constructor(message: string = 'Forbidden') {
+    super(message, 403);
+  }
+}
+
+export class NotFoundError extends AppError {
+  constructor(message: string = 'Not found') {
+    super(message, 404);
+  }
+}
+
+export class ConflictError extends AppError {
+  constructor(message: string) {
+    super(message, 409);
+  }
+}
