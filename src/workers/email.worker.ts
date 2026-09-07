819eda402918a2739ebf16c94dbffa53d05df96c
israeldewcom <haddockthor@gmail.com>
2026-09-07 13:17:10 +0100

    Rename email worker queue from 'schoolflow:email' to 'schoolflow_email'

--- src/workers/email.worker.ts
+++ src/workers/email.worker.ts
@@ -3,7 +3,7 @@ import { redis } from '../config/redis';
 import { sendEmail } from '../integrations/email/nodemailer';
 import logger from '../config/logger';
 
-const worker = new Worker('schoolflow:email', async (job) => {
+const worker = new Worker('schoolflow_email', async (job) => {
   const { to, subject, html, text } = job.data;
   await sendEmail(to, subject, html, text);
 }, {
