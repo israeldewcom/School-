52cf370b92751bd7f7945d71b39839393994e462
israeldewcom <haddockthor@gmail.com>
2026-09-12 12:02:30 +0100

    Pass user context to UserService methods

--- src/core/users/user.controller.ts
+++ src/core/users/user.controller.ts
@@ -4,7 +4,7 @@ import { UserService } from './user.service';
 export class UserController {
   static async create(req: Request, res: Response, next: NextFunction) {
     try {
-      const user = await UserService.create(req.body);
+      const user = await UserService.create(req.body, req.user);
       res.status(201).json({ success: true, data: user });
     } catch (error) {
       next(error);
@@ -13,7 +13,7 @@ export class UserController {
 
   static async getUsers(req: Request, res: Response, next: NextFunction) {
     try {
-      const users = await UserService.getAll(req.query);
+      const users = await UserService.getAll(req.query, req.user);
       res.json({ success: true, data: users });
     } catch (error) {
       next(error);
@@ -22,7 +22,7 @@ export class UserController {
 
   static async getUser(req: Request, res: Response, next: NextFunction) {
     try {
-      const user = await UserService.getById(req.params.id);
+      const user = await UserService.getById(req.params.id, req.user);
       res.json({ success: true, data: user });
     } catch (error) {
       next(error);
@@ -31,7 +31,7 @@ export class UserController {
 
   static async update(req: Request, res: Response, next: NextFunction) {
     try {
-      const user = await UserService.update(req.params.id, req.body);
+      const user = await UserService.update(req.params.id, req.body, req.user);
       res.json({ success: true, data: user });
     } catch (error) {
       next(error);
@@ -40,7 +40,7 @@ export class UserController {
 
   static async delete(req: Request, res: Response, next: NextFunction) {
     try {
-      await UserService.delete(req.params.id);
+      await UserService.delete(req.params.id, req.user);
       res.json({ success: true, message: 'User deleted' });
     } catch (error) {
       next(error);
