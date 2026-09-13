3513b7b6faad53da86f01100d762aca4b5282b9c
israeldewcom <haddockthor@gmail.com>
2026-09-13 17:48:58 +0100

    Refactor json-logic-js type definitions

--- src/types/json-logic-js.d.ts
+++ src/types/json-logic-js.d.ts
@@ -1,4 +1,13 @@
 declare module 'json-logic-js' {
-  const jsonLogic: any;
-  export = jsonLogic;
+  export function apply(logic: any, data: any): any;
+  export function add_operation(name: string, fn: (...args: any[]) => any): void;
+  export function is_logic(value: any): boolean;
+  export function truthy(value: any): boolean;
+  const _default: {
+    apply: typeof apply;
+    add_operation: typeof add_operation;
+    is_logic: typeof is_logic;
+    truthy: typeof truthy;
+  };
+  export default _default;
 }
