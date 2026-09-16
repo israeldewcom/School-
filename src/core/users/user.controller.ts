083ebf272d1c06fe05357cd1f980dd2c434dcc6b
israeldewcom <haddockthor@gmail.com>
2026-09-16 08:34:16 +0100

    Refactor UserController methods for schoolId and userId

--- src/core/users/user.controller.ts
+++ src/core/users/user.controller.ts
@@ -1,49 +1,50 @@
+// src/core/users/user.controller.ts
 import { Request, Response, NextFunction } from 'express';
 import { UserService } from './user.service';
 
 export class UserController {
+  static async list(req: Request, res: Response, next: NextFunction) {
+    try {
+      const users = await UserService.list(req.schoolId!, req.query);
+      res.json({ success: true, data: users });
+    } catch (err) { next(err); }
+  }
+
   static async create(req: Request, res: Response, next: NextFunction) {
     try {
-      const user = await UserService.create(req.body, req.user);
+      const user = await UserService.create(req.schoolId!, req.userId!, req.body);
       res.status(201).json({ success: true, data: user });
-    } catch (error) {
-      next(error);
-    }
-  }
-
-  static async getUsers(req: Request, res: Response, next: NextFunction) {
-    try {
-      const users = await UserService.getAll(req.query, req.user);
-      res.json({ success: true, data: users });
-    } catch (error) {
-      next(error);
-    }
-  }
-
-  static async getUser(req: Request, res: Response, next: NextFunction) {
-    try {
-      const user = await UserService.getById(req.params.id, req.user);
-      res.json({ success: true, data: user });
-    } catch (error) {
-      next(error);
-    }
+    } catch (err) { next(err); }
   }
 
   static async update(req: Request, res: Response, next: NextFunction) {
     try {
-      const user = await UserService.update(req.params.id, req.body, req.user);
+      const user = await UserService.update(
+        req.schoolId!,
+        req.userId!,
+        req.params.id,
+        req.body
+      );
       res.json({ success: true, data: user });
-    } catch (error) {
-      next(error);
-    }
+    } catch (err) { next(err); }
   }
 
   static async delete(req: Request, res: Response, next: NextFunction) {
     try {
-      await UserService.delete(req.params.id, req.user);
-      res.json({ success: true, message: 'User deleted' });
-    } catch (error) {
-      next(error);
-    }
+      const result = await UserService.delete(req.schoolId!, req.userId!, req.params.id);
+      res.json({ success: true, data: result });
+    } catch (err) { next(err); }
+  }
+
+  static async resetPassword(req: Request, res: Response, next: NextFunction) {
+    try {
+      const result = await UserService.resetPassword(
+        req.schoolId!,
+        req.userId!,
+        req.params.id,
+        req.body.password
+      );
+      res.json({ success: true, data: result });
+    } catch (err) { next(err); }
   }
 }
