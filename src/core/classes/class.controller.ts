793b23f8609d34281a2adab8738d73152f82736e
israeldewcom <haddockthor@gmail.com>
2026-09-19 15:07:31 +0100

    Refactor ClassController methods for listing and creating classes

--- src/core/classes/class.controller.ts
+++ src/core/classes/class.controller.ts
@@ -1,50 +1,40 @@
+// src/core/classes/class.controller.ts
 import { Request, Response, NextFunction } from 'express';
 import { ClassService } from './class.service';
 
 export class ClassController {
+  static async list(req: Request, res: Response, next: NextFunction) {
+    try {
+      const classes = await ClassService.list(req.schoolId);
+      res.json({ success: true, data: classes });
+    } catch (err) { next(err); }
+  }
+
+  static async getById(req: Request, res: Response, next: NextFunction) {
+    try {
+      const cls = await ClassService.getById(req.schoolId, req.params.id);
+      res.json({ success: true, data: cls });
+    } catch (err) { next(err); }
+  }
+
   static async create(req: Request, res: Response, next: NextFunction) {
     try {
-      const data = { ...req.body, schoolId: req.schoolId };
-      const cls = await ClassService.create(data);
+      const cls = await ClassService.create(req.schoolId, req.body);
       res.status(201).json({ success: true, data: cls });
-    } catch (error) {
-      next(error);
-    }
-  }
-
-  static async getClasses(req: Request, res: Response, next: NextFunction) {
-    try {
-      const classes = await ClassService.getAll(req.schoolId!, req.query);
-      res.json({ success: true, data: classes });
-    } catch (error) {
-      next(error);
-    }
-  }
-
-  static async getClass(req: Request, res: Response, next: NextFunction) {
-    try {
-      const cls = await ClassService.getById(req.params.id, req.schoolId!);
-      res.json({ success: true, data: cls });
-    } catch (error) {
-      next(error);
-    }
+    } catch (err) { next(err); }
   }
 
   static async update(req: Request, res: Response, next: NextFunction) {
     try {
-      const cls = await ClassService.update(req.params.id, req.schoolId!, req.body);
+      const cls = await ClassService.update(req.schoolId, req.params.id, req.body);
       res.json({ success: true, data: cls });
-    } catch (error) {
-      next(error);
-    }
+    } catch (err) { next(err); }
   }
 
   static async delete(req: Request, res: Response, next: NextFunction) {
     try {
-      await ClassService.delete(req.params.id, req.schoolId!);
-      res.json({ success: true, message: 'Class deleted' });
-    } catch (error) {
-      next(error);
-    }
+      const result = await ClassService.delete(req.schoolId, req.params.id);
+      res.json({ success: true, data: result });
+    } catch (err) { next(err); }
   }
 }
